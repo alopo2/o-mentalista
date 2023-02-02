fd80a2f6403799bf0ab087d5b2893b182b339986
Angela Carvalho <angelalopo@outlook.com>
2023-02-02 10:42:28 -0300

    adiciona condicionais para testar o chute

--- js/index.js
+++ js/index.js
@@ -2,4 +2,12 @@ let numeroSecreto = Math.random(10);
 
 let chute = prompt("Digite um numero: ")
 
+if (chute == numeroSecreto) {
+    console.log("Acertou!")
+} else if (chute > numeroSecreto) {
+    console.log("Errou. O numero secreto é menor")
+} else if (chute < numeroSecreto) {
+    console.log("Errou. O numero secreto é maior")
+}
+
 console.log(numeroSecreto)
