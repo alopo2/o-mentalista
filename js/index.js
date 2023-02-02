77c566212597493f58414b952c173d8069062c92
Angela Carvalho <angelalopo@outlook.com>
2023-02-02 10:43:26 -0300

    adiciona loop while

--- js/index.js
+++ js/index.js
@@ -2,12 +2,14 @@ let numeroSecreto = Math.random(10);
 
 let chute = prompt("Digite um numero: ")
 
-if (chute == numeroSecreto) {
-    console.log("Acertou!")
-} else if (chute > numeroSecreto) {
-    console.log("Errou. O numero secreto é menor")
-} else if (chute < numeroSecreto) {
-    console.log("Errou. O numero secreto é maior")
+while (chute != numeroSecreto) {
+    if (chute == numeroSecreto) {
+        console.log("Acertou!")
+    } else if (chute > numeroSecreto) {
+        console.log("Errou. O numero secreto é menor")
+    } else if (chute < numeroSecreto) {
+        console.log("Errou. O numero secreto é maior")
+    }
 }
 
 console.log(numeroSecreto)
